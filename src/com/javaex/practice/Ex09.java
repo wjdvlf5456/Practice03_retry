4a3f39f6b41e3a2267cd124f3a6f3b43f640b25b
최정필 <wjdvlf1407@gmail.com>
2022-04-19 16:29:33 +0900

    Ex09 문제풀기

---
+++ src/com/javaex/practice/Ex09.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 11; i++) {
+
+			for (int j = 0; j < 10; j++) {
+				System.out.print(j + i + "\t");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
