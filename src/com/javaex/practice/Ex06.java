1630c40bbc17e53d537dcf0b1b64373e71dd9e5f
최정필 <wjdvlf1407@gmail.com>
2022-04-19 16:13:48 +0900

    Ex06 주석

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -4,8 +4,9 @@ public class Ex06 {
 
 	public static void main(String[] args) {
 
-		for (int i = 1; i <= 100; i++) {
-			if (i % 5 == 0 && i % 7 == 0) {
+		for (int i = 1; i <= 100; i++) {		//	1에서 100까지
+			
+			if (i % 5 == 0 && i % 7 == 0) {		//	5의 배수이면서 7의 배수
 				System.out.println(i);
 			}
 
