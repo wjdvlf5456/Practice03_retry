9c9b427609da6bf633b86aac1e6a776ace809b72
최정필 <wjdvlf1407@gmail.com>
2022-04-19 16:33:45 +0900

    Ex10 문제풀기

---
+++ src/com/javaex/practice/Ex10.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("숫자를 입력하세요");
+
+		int max = 0;
+		for (int i = 1; i < 6; i++) {
+
+			System.out.print("숫자: ");
+			int num = sc.nextInt();
+
+			if (num > max) {
+				max = num;
+			}
+		}
+		System.out.println("최대값은 " + max + " 입니다.");
+
+		sc.close();
+
+	}
+
+}
