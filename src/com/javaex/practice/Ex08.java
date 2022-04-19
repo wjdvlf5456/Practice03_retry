206f79449de9e8651c6dc5412ace3f69593434e3
최정필 <wjdvlf1407@gmail.com>
2022-04-19 16:27:20 +0900

    Ex08 문제풀기

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+public class Ex08 {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <10 ; i++) {
+			
+			for (int j = 2; j <10 ; j++) {
+				System.out.print(j+"*"+i+"="+i*j+"\t");
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
